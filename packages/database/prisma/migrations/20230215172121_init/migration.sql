1baca5b2471896e83f6b3a7ce74a016eb8b44c18
dheeraj <dheeraj.iitb2006@gmail.com>
2023-02-15 09:29:17 -0800

    first commit

---
+++ packages/database/prisma/migrations/20230215172121_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Article" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
